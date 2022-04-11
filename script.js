408db252dfa55c3e6627ac5831414135c47a87e2
Pedro Henrique Lourenço Ferezin <phlferezin03@gmail.com>
2022-04-11 20:00:56 -0300

    Logo atualizada e header finalizado

---
+++ script.js
@@ -0,0 +1,7 @@
+let btnMobile = document.getElementById('btnMobile');
+let nav = document.getElementById('nav')
+let menu = document.getElementById('menu');
+
+btnMobile.addEventListener('click', () =>{
+    nav.classList.toggle('active')
+})
